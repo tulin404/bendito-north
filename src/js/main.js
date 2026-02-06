42aec5df0b0fbed29835bf058ee5ed7b1e6734db
Túlio <tuliooliveirafilipin10@gmail.com>
2026-02-06 18:20:40 -0300

    hero changes

--- src/js/main.js
+++ src/js/main.js
@@ -5,11 +5,11 @@ const knowMoreText = document.getElementById('know-more-text');
 knowMoreBtn.addEventListener('pointerenter', () => {
     knowMoreArrow.classList.toggle('-translate-y-48');
     knowMoreArrow.classList.toggle('-translate-y-1/2');
-    knowMoreText.classList.toggle('translate-y-12');
+    knowMoreText.classList.toggle('translate-y-24');
 });
 
 knowMoreBtn.addEventListener('pointerleave', () => {
     knowMoreArrow.classList.toggle('-translate-y-48');
     knowMoreArrow.classList.toggle('-translate-y-1/2');
-    knowMoreText.classList.toggle('translate-y-12');
+    knowMoreText.classList.toggle('translate-y-24');
 });
