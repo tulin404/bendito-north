28d4311d560edd1c49b8f8b113a3ea69b366f03e
Tulio <221194504+tulin404@users.noreply.github.com>
2026-02-09 18:08:17 -0300

    Finished benefits proto

--- src/js/main.js
+++ src/js/main.js
@@ -13,3 +13,17 @@ knowMoreBtn.addEventListener('pointerleave', () => {
     knowMoreArrow.classList.toggle('-translate-y-1/2');
     knowMoreText.classList.toggle('translate-y-24');
 });
+
+const mainText = document.getElementById('main-hero-text');
+const benditoArray = ['B', 'E', 'N', 'D', 'I', 'T', 'O', ' ', 'N', 'O', 'R', 'T', 'H'];
+window.addEventListener('load', () => {
+    benditoArray.forEach((letter, index) => {
+        setTimeout(() => {
+            mainText.append(letter)
+        }, index * 150)
+    });
+
+    setInterval(() => {
+        mainText.classList.toggle('pseudo');
+    }, 600);
+});
