b0a0663e523208a2e0d89679c4c84c087d9bec87
Tulio Oliveira <tuliooliveirafilipin10@gmail.com>
2026-03-12 00:06:57 -0300

    Remade LP - Header, hero and Present

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,53 @@
-const knowMoreBtn = document.getElementById('know-more');
-const knowMoreArrow = document.getElementById('arrow-down');
-const knowMoreText = document.getElementById('know-more-text');
+const hamMenuBtn = document.getElementById('ham-menu-btn');
+const hamMenu = document.getElementById('ham-menu');
 
-knowMoreBtn.addEventListener('pointerenter', () => {
-    knowMoreArrow.classList.toggle('-translate-y-48');
-    knowMoreArrow.classList.toggle('-translate-y-1/2');
-    knowMoreText.classList.toggle('translate-y-24');
+hamMenuBtn.addEventListener('click', () => {
+    hamMenuBtn.classList.toggle('active');
+    hamMenu.classList.toggle('active');
+    hamMenu.classList.toggle('-translate-x-[100dvw]');
+    document.documentElement.classList.toggle('op-blur');
 });
 
-knowMoreBtn.addEventListener('pointerleave', () => {
-    knowMoreArrow.classList.toggle('-translate-y-48');
-    knowMoreArrow.classList.toggle('-translate-y-1/2');
-    knowMoreText.classList.toggle('translate-y-24');
+if (innerWidth > 640) {
+    hamMenu.inert = true;
+}
+
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    if (scrollY > 0) {
+        header.classList.add('scale-y-85');
+    } else {
+        header.classList.remove('scale-y-85')
+    }
 });
 
-const mainText = document.getElementById('main-hero-text');
-const benditoArray = ['B', 'E', 'N', 'D', 'I', 'T', 'O', ' ', 'N', 'O', 'R', 'T', 'H'];
-window.addEventListener('load', () => {
-    benditoArray.forEach((letter, index) => {
-        setTimeout(() => {
-            mainText.append(letter)
-        }, index * 150)
-    });
+const wrapper = document.querySelector("#icons-wrapper");
+const track = document.querySelector("#track");
+const slides = document.querySelectorAll(".vision");
 
-    setInterval(() => {
-        mainText.classList.toggle('pseudo');
-    }, 600);
-});
+let index = 0;
+
+function moveToSlide() {
+    if (index === 0) {
+        track.style.transform = 'translateX(0)'
+    } else {
+        const slide = slides[index];
+    
+        const slideCenter = slide.offsetLeft + slide.clientWidth / 2;
+        const wrapperCenter = wrapper.clientWidth / 2;
+    
+        const offset = slideCenter - wrapperCenter;
+    
+        track.style.transform = `translateX(-${offset}px)`;
+    };
+};
+
+setInterval(() => {
+    index++;
+    if (index >= slides.length) {
+        index = 0;
+    };
+    moveToSlide();
+}, 5000);
+
